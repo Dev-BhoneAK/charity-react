4c9abe3cf8b7842dfda58bd4fdccf5f9e9f1e153
BhoneAK <bhoneak@outlook.com>
2022-09-14 23:30:02 +0630

    feat: add actions

---
+++ src/actions/donations.js
@@ -0,0 +1,21 @@
+import {getDonations} from '../utils/api';
+import {showLoading, hideLoading} from "./loading";
+
+export const RECEIVE_DONATIONS = 'RECEIVE_DONATIONS';
+
+const receiveDonations = (returnDataArr) => {
+    return {
+        type: RECEIVE_DONATIONS,
+        donations: returnDataArr
+    }
+}
+
+export const getDonationData = () => {
+    return (dispatch) => {
+        dispatch(showLoading());
+        getDonations().then((returnDataArr) => {
+            dispatch(receiveDonations(returnDataArr));
+            dispatch(hideLoading());
+        });
+    }
+}
